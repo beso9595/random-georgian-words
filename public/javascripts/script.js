e6d65290b3f302d9c656a6ac4c11e884e339aa9a
Beso Bantsadze <beso@fina2.net>
2017-09-15 13:09:27 +0400

    added ajax request, switched from jade to ejs, code format

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,19 @@
-alert(1);
-// $(document).ready(function () {
-// 	alert(1);
-// 	$("#generate").click(function () {
-// 		alert(2);
-// 		$.ajax({
-// 			type: 'POST',
-// 			url: '/',
-// 			contentType: 'application/json',
-// 			data: '{"command": "on"}',
-// 			dataType: 'json',
-// 			success: function (data) {
-// 				console.log(data);
-// 			},
-// 			error: function () {
-// 				console.log('fail');
-// 			}
-// 		});
-// 	});
-// });
+
+$(document).ready(function () {
+	$("#generate").click(function () {
+		$.ajax({
+			type: 'POST',
+			url: '/',
+			contentType: 'application/json',
+			dataType: 'json',
+			success: function (res) {
+				$("#randomWord").text(res.randomWord);
+			},
+			error: function (res) {
+				console.log('error');
+				console.log(res);
+				$("#randomWord").text("");
+			}
+		});
+	});
+});
--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,10 @@ router.post('/', function (req, res) {
 	var randomWordServiceUrl = 'http://watchout4snakes.com/wo4snakes/Random/RandomWord',
 		translateWordServiceUrl = 'https://www.translate.com/translator/ajax_translate';
 
-	var randomWord = generate(randomWordServiceUrl, translateWordServiceUrl, function(randomWord){
-		res.render('index', {
-			title: 'Title',
+	generate(randomWordServiceUrl, translateWordServiceUrl, function (randomWord) {
+		res.send({
 			randomWord: randomWord
-		});
+		})
 	});
 
 });
@@ -33,9 +32,9 @@ function generate(randomWordServiceUrl, translateWordServiceUrl, next) {
 				},
 				function (error, response, body) {
 					if (!error && response.statusCode === 200) {
-						var parsedBody = JSON.parse(body);
-						var translatedWord = parsedBody['translated_text'];
-						return next(translatedWord);
+						var parsedBody = JSON.parse(body),
+							translatedWord = parsedBody['translated_text'];
+						return validateWord(translatedWord) ? next(translatedWord) : generate(randomWordServiceUrl, translateWordServiceUrl);
 					} else {
 						console.log('another try of translate');
 						return generate(randomWordServiceUrl, translateWordServiceUrl);
@@ -49,10 +48,8 @@ function generate(randomWordServiceUrl, translateWordServiceUrl, next) {
 	});
 }
 
-function asd(a,b,next){
-	//..
-	//..
-	next();
+function validateWord(randomWord) {
+	return randomWord.match(/[a-z]/i) === null;
 }
 
 module.exports = router;
