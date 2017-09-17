b505ee739d500769fabf8fc4606b6f6339a7f8c1
Beso Bantsadze <beso@fina2.net>
2017-09-17 22:52:16 +0400

    started word generating development

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,19 +1,39 @@
-
 $(document).ready(function () {
+
+	var types = ['n', 'a', 't', 'i', 'e', 'z', 's'];
+
+
 	$("#generate").click(function () {
+		var params = {
+			quantity: $("#quantity").val(),
+			type: $("#type").is(':checked')
+		};
+		types.forEach(function(t){
+			params['type_' + t] = $('#type_' + t).is(':checked');
+		});
+
 		$.ajax({
 			type: 'POST',
 			url: '/',
 			contentType: 'application/json',
 			dataType: 'json',
+			data: JSON.stringify(params),
 			success: function (res) {
-				$("#randomWord").text(res.randomWord);
+				$("#randomWords").text(res.randomWords);
 			},
 			error: function (res) {
 				console.log('error');
 				console.log(res);
-				$("#randomWord").text("");
+				$("#randomWords").text("");
 			}
 		});
 	});
+
+	$("#type").click(function () {
+		var disable = $("#type").is(':checked');
+		types.forEach(function(t){
+			$('#type_' + t).attr('disabled', disable);
+		});
+	});
+
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
-var request = require('request');
 var router = express.Router();
+
+var redis = require("redis"),
+	client = redis.createClient();
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -9,47 +11,100 @@ router.get('/', function (req, res) {
 
 /* POST generate random word. */
 router.post('/', function (req, res) {
-	var randomWordServiceUrl = 'http://watchout4snakes.com/wo4snakes/Random/RandomWord',
-		translateWordServiceUrl = 'https://www.translate.com/translator/ajax_translate';
-
-	generate(randomWordServiceUrl, translateWordServiceUrl, function (randomWord) {
-		res.send({
-			randomWord: randomWord
-		})
-	});
-
+	var params = req.body,
+		quantity = params['quantity'] | 1,
+		randomWords;
+	if (params) {
+		if (params['type']) {
+			randomWords = getRandomWords(quantity);
+		} else {
+			var tps = ['n', 'a', 't', 'i', 'e', 'z', 's'],
+				types = [];
+			tps.forEach(function (t) {
+				if (params['type_' + t]) {
+					types.push(t);
+				}
+			});
+			randomWords = getRandomWords(quantity, types);
+		}
+	}
+	res.send({
+		randomWords: randomWords
+	})
 });
 
-function generate(randomWordServiceUrl, translateWordServiceUrl, next) {
-	request.post(randomWordServiceUrl, function (error, response, body) {
-		if (!error && response.statusCode === 200) {
-			request.post(translateWordServiceUrl, {
-					form: {
-						text_to_translate: body,
-						source_lang: 'en',
-						translated_lang: 'ka'
-					}
-				},
-				function (error, response, body) {
-					if (!error && response.statusCode === 200) {
-						var parsedBody = JSON.parse(body),
-							translatedWord = parsedBody['translated_text'];
-						return validateWord(translatedWord) ? next(translatedWord) : generate(randomWordServiceUrl, translateWordServiceUrl);
-					} else {
-						console.log('another try of translate');
-						return generate(randomWordServiceUrl, translateWordServiceUrl);
-					}
+function getRandomWords(quantity, types) {
+	var generatedWords = [];
+	if (types) {
+		var tables = [];
+		var typesWithQuantity = distribute(quantity, types);
+		typesWithQuantity.forEach(function (twq, i) {
+			generate(twq.quantity, twq.type, generatedWords);
+		});
+	} else {
+
+	}
+	return generatedWords;
+}
+
+function generate(quantity, type, generatedWords) {
+	var pre = 'rgw_',
+		randomNumbers = [],
+		index;
+	client.hget((pre + 'indexes'), type, function (err, typeIndex) {
+		if (!err && typeIndex) {
+			while (quantity-- > 0) {
+				index = Math.floor(Math.random() * typeIndex) + 1;
+				if (!randomNumbers.includes(index)) {
+					randomNumbers.push(index);
 				}
-			);
-		} else {
-			console.log('another try of generate');
-			return generate(randomWordServiceUrl, translateWordServiceUrl);
+			}
+			//TODO
 		}
 	});
 }
 
-function validateWord(randomWord) {
-	return randomWord.match(/[a-z]/i) === null;
+function distribute(quantity, types) {
+	var typesLength = types.length,
+		distributed = [],
+		chosen = [],
+		index;
+	if (quantity < typesLength) {
+		while (chosen.length < quantity) {
+			index = Math.floor(Math.random() * typesLength);
+			if (!chosen.includes(types[index])) {
+				chosen.push(types[index]);
+			}
+		}
+		chosen.forEach(function (c) {
+			distributed.push({
+				type: c,
+				quantity: 1
+			});
+		})
+	} else if (quantity > typesLength) {
+		var per = Math.floor(quantity / typesLength),
+			extra = quantity % typesLength;
+		while (chosen.length < extra) {
+			index = Math.floor(Math.random() * typesLength);
+			if (!chosen.includes(index)) {
+				chosen.push(index);
+			}
+		}
+		types.forEach(function (t, i) {
+			distributed.push({
+				type: t,
+				quantity: per + (chosen.includes(i) ? 1 : 0)
+			});
+		});
+	} else {
+		types.forEach(function (t) {
+			distributed.push({
+				type: t,
+				quantity: 1
+			});
+		});
+	}
 }
 
 module.exports = router;
